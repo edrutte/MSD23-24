7d6794cca86e4eec1ce315d30ac59d74e082ffb4
Evan Ruttenberg <70585539+edrutte@users.noreply.github.com>
2024-01-31 13:47:18 -0500

    Simplify Stockfish stdout read

--- src/include/main.h
+++ src/include/main.h
@@ -1,4 +1,3 @@
 #ifndef CHESS_MAIN_H
 #define CHESS_MAIN_H
-const struct timespec pipe_delay = {.tv_nsec = 200000000};
-#endif // CHESS_MAIN_H
+#endif// CHESS_MAIN_H
--- src/lib/main.c
+++ src/lib/main.c
@@ -3,12 +3,22 @@
 #endif
 #include <fcntl.h>
 #include <gpiod.h>
+#include <limits.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
 #include "main.h"
+
+_Noreturn void cleanup_and_die(int num_fd, ...) {
+	va_list to_close;
+	va_start(to_close, num_fd);
+	for (int i = 0; i < num_fd; i++) {
+		close(va_arg(to_close, int));
+	}
+	exit(EXIT_FAILURE);
+}
 
 /**
  * Parts taken from https://stackoverflow.com/questions/34348111/c-redirecting-stdout-after-forking-of-child-from-parent
@@ -33,38 +43,40 @@ int main(int argc, char* argv[]) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
-    if (pipe2(fish_stdout_pipefd, O_NONBLOCK) == -1) {
+    if (pipe2(fish_stdout_pipefd, O_DIRECT) == -1) {
         perror("pipe");
-        exit(EXIT_FAILURE);
+	    cleanup_and_die(2, fish_stdin_pipefd[0], fish_stdin_pipefd[1]);
     }
     pid = fork();
     switch (pid) {
         case -1:
             perror("fork");
-            exit(EXIT_FAILURE);
+		    cleanup_and_die(4, fish_stdin_pipefd[0], fish_stdin_pipefd[1], fish_stdout_pipefd[0], fish_stdout_pipefd[1]);
         case 0:
+			close(fish_stdout_pipefd[0]);
+		    close(fish_stdin_pipefd[1]);
             if (dup2(fish_stdin_pipefd[0], STDIN_FILENO) == -1) {
                 perror("dup");
-                exit(EXIT_FAILURE);
+	            cleanup_and_die(2, fish_stdin_pipefd[0], fish_stdout_pipefd[1]);
             }
             if (dup2(fish_stdout_pipefd[1], STDOUT_FILENO) == -1) {
                 perror("dup");
-                exit(EXIT_FAILURE);
+	            cleanup_and_die(2, fish_stdin_pipefd[0], fish_stdout_pipefd[1]);
             }
             execv(argv[1], fishargv);
             perror("execv");
-            exit(EXIT_FAILURE);
+		    cleanup_and_die(2, fish_stdin_pipefd[0], fish_stdout_pipefd[1]);
         default:
-            break;
+	        close(fish_stdout_pipefd[1]);
+		    close(fish_stdin_pipefd[0]);
     }
+	char fish[PIPE_BUF] = {0};
+	read(fish_stdout_pipefd[0], fish, 1);// Get rid of Stockfish intro
     write(fish_stdin_pipefd[1], "uci", 3);
-    nanosleep(&pipe_delay, NULL);
-    char fish[256] = {0};
-	ssize_t n_read = read(fish_stdout_pipefd[0], fish, 256);
-	while (n_read > 0) {
-		printf("%.*s", (int) n_read, fish);
-		n_read = read(fish_stdout_pipefd[0], fish, 256);
-	}
-    // Maybe call wait()
+	ssize_t n_read = read(fish_stdout_pipefd[0], fish, PIPE_BUF);
+	printf("%.*s", (int) n_read, fish);
+    close(fish_stdin_pipefd[1]);
+	close(fish_stdout_pipefd[0]);
+	// Maybe call wait()
 	return EXIT_SUCCESS;
 }
