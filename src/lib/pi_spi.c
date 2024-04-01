f61c1e74711e51f39165008dca91ff61a70cb8e6
Evan Ruttenberg <70585539+edrutte@users.noreply.github.com>
2024-04-01 12:28:57 -0400

    Initialize all fields of spi_ioc_transfer

--- src/lib/pi_spi.c
+++ src/lib/pi_spi.c
@@ -3,7 +3,7 @@
 #include "pi_spi.h"
 
 int send_spi(int fd, uint8_t const *tx, uint8_t *rx, size_t len) {
-	struct spi_ioc_transfer spi_tx = {.tx_buf = (unsigned long) tx, .rx_buf = (unsigned long) rx, .len = len};
+	struct spi_ioc_transfer spi_tx = {.tx_buf = (unsigned long) tx, .rx_buf = (unsigned long) rx, .len = len, .bits_per_word = 8, .speed_hz = 10000000, .delay_usecs = 0, .tx_nbits = 0, .rx_nbits = 0};
 	if (ioctl(fd, SPI_IOC_MESSAGE(1), &spi_tx) < 1) {
 		perror("send_spi");
 		return 1;
